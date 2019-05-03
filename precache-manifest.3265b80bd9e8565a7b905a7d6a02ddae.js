3bd0c91f6b4ed312c0990217ed37184b7339eb4c
Fredrik V. Mørken <fredrik@valdmanis.com>
2019-05-03 13:05:48 +0200

    Updates

---
+++ precache-manifest.3265b80bd9e8565a7b905a7d6a02ddae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52200e363d7731fc7db2534f76a1ea6",
+    "url": "/fridager/index.html"
+  },
+  {
+    "revision": "4211cce9876e907bba67",
+    "url": "/fridager/static/css/main.cb937da8.chunk.css"
+  },
+  {
+    "revision": "02d37a77630ad2877999",
+    "url": "/fridager/static/js/2.8a63cc60.chunk.js"
+  },
+  {
+    "revision": "4211cce9876e907bba67",
+    "url": "/fridager/static/js/main.d8eb2017.chunk.js"
+  },
+  {
+    "revision": "2906b3dc8e001ac975ce",
+    "url": "/fridager/static/js/runtime~main.a55fefca.js"
+  }
+]);
